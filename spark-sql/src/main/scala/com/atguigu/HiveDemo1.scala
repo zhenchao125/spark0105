c6e746d0605c18f728d8febc1003a0b9a0d2f4b8
John Doe <30752740@qq.com>
2019-06-05 14:29:46 +0800

    update

---
+++ spark-sql/src/main/scala/com/atguigu/HiveDemo1.scala
@@ -0,0 +1,27 @@
+package com.atguigu
+
+import org.apache.spark.sql.SparkSession
+
+object HiveDemo1 {
+    def main(args: Array[String]): Unit = {
+        val spark: SparkSession = SparkSession
+            .builder()
+            .master("local[*]")
+            .appName("Test")
+            // 添加支持外置的 hive
+            .enableHiveSupport()
+            .config("spark.sql.warehouse.dir", "hdfs://hadoop201:9000/user/hive/warehouse")
+            .getOrCreate()
+        import spark.implicits._
+        
+        spark.sql("show tables").show
+//        spark.sql("create table test(id int)").show
+        spark.sql("create database spark0107").show
+        spark.sql("use spark0107").show
+        spark.sql("create table test(id int)").show
+        spark.sql("insert into test values(1)").show
+        spark.sql("insert into test values(2)").show
+        
+        spark.stop()
+    }
+}
